b32b7d00447ccc7afbdf62fe1f24e3a70b0b5802
420kyle69 <66268622+420kyle69@users.noreply.github.com>
2024-03-11 17:35:31 -0700

    Change output folder to 'docs'

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const config = {
   output: {
     // Compile the source files into a bundle.
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   // Enable webpack-dev-server to get hot refresh of the app.
